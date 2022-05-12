65af1a6a053f8dabbcba341a8a1a4be07e145ffd
Anup <anuppoudel60@gmail.com>
2022-05-12 11:53:41 +1000

    chat box created

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 
+const users = [{}];
+
 app.get("/", (req, res) => {
   res.send("Its working!");
 });
@@ -16,8 +18,20 @@ const server = http.createServer(app);
 
 const io = socketIO(server);
 
-io.on("connect", () => {
+io.on("connection", (socket) => {
   console.log("New connection");
+
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined`);
+
+    socket.broadcast.emit("userJoined", {
+      user: "Admin",
+      message: `${users[socket.id]} has joined`,
+    });
+  });
+
+  socket.emit("Welcome", { user: "Admin", message: "Welcome to the chat" });
 });
 
 server.listen(PORT, () => {
